cf5e03aeb4780bea02309104769b91b605ca5b65
Jonathan Palalay <ring5pun@hotmail.com>
2022-04-12 23:43:25 -0500

    added and removed the first item of favorite food array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -70,7 +70,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favFoods.unshift('pizza');
+console.log(`Added a favorite food to beginning: ${favFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -79,10 +80,17 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = favFoods.shift();
+console.log('Removed the first favorite food', removedFood);
+console.log(' The favorite foods are now', favFoods);
+
+
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+
+
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
